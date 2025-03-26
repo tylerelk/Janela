bb6b731bf32b5d20e8b18f24eb7d083e3f1c22fe
Tyler Lewis <tylerlewisrn@gmail.com>
2025-03-26 15:56:08 -0400

    Update mobile styles; (KNOWN BUG: Current location not working as expected)

--- Janela/Data/Globals.cs
+++ Janela/Data/Globals.cs
@@ -1,5 +1,7 @@
+using System.Runtime.CompilerServices;
 using Blazorise;
 using Alert = Janela.Data.Services.Classes.Alert;
+using Wangkanai.Detection.Services;
 
 namespace Janela.Data;
 
@@ -12,11 +14,18 @@ public class Globals
     public static int ColdPoint { get; set; } = 7;
     public static int HotPoint { get; set; } = 27;
     public static Dictionary<string, string> BgImageUrls { get; set; } = new();
+    public static bool IsMobile { get; set; }
 
     //Utility to show correct units based on user preference
     public static string ForC()
     {
         var result = UseF ? "\u00b0F" : "\u00b0C";
         return result;
+    }
+    
+    //This method run at page load via Home.razor to set mobile detection
+    public static void detectMobile(bool isMobile)
+    {
+        IsMobile = isMobile;
     }
 }
